38120f79df44fa3ff3f0f4b27562b4569adaff2b
katsuyorii <katsuyoriiii@gmail.com>
2025-07-12 13:58:49 +0300

    Create jwt tokens service

--- backend/auth/dependencies.py
+++ backend/auth/dependencies.py
@@ -3,8 +3,11 @@ from fastapi import Depends
 from users.repositories import UsersRepository
 from users.dependencies import get_users_repository
 
-from .services import AuthService
+from .services import AuthService, JWTTokensService
 
 
-def get_auth_service(users_repository: UsersRepository = Depends(get_users_repository)) -> AuthService:
-    return AuthService(users_repository)
+def get_jwt_tokens_service() -> JWTTokensService:
+    return JWTTokensService()
+
+def get_auth_service(users_repository: UsersRepository = Depends(get_users_repository), jwt_tokens_service: JWTTokensService = Depends(get_jwt_tokens_service)) -> AuthService:
+    return AuthService(users_repository, jwt_tokens_service)
